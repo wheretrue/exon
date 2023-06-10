9466cb7063413f2d91cecbebf5b2e9a51bfe0f9a
Trent Hauck <trent@trenthauck.com>
2023-06-09 20:01:55 -0700

    test: better tests for vcf parsing

--- exon/src/datasources/vcf/schema_builder.rs
+++ exon/src/datasources/vcf/schema_builder.rs
@@ -29,6 +29,12 @@ pub struct VCFSchemaBuilder {
     fields: Vec<arrow::datatypes::Field>,
 }
 
+impl Default for VCFSchemaBuilder {
+    fn default() -> Self {
+        Self::new()
+    }
+}
+
 impl VCFSchemaBuilder {
     /// Creates a new VCF schema builder.
     pub fn new() -> Self {
@@ -91,13 +97,6 @@ impl From<Header> for VCFSchemaBuilder {
     }
 }
 
-impl Default for VCFSchemaBuilder {
-    fn default() -> Self {
-        // Make all strings
-        todo!("default")
-    }
-}
-
 fn vcf_info_type_to_arrow_type(ty: InfoType) -> arrow::datatypes::DataType {
     match ty {
         InfoType::Integer => arrow::datatypes::DataType::Int32,
@@ -123,7 +122,11 @@ fn wrap_type_in_count(cnt: InfoNumber, typ: &arrow::datatypes::Field) -> arrow::
         InfoNumber::Count(1) => typ.clone(),
         _ => arrow::datatypes::Field::new(
             typ.name(),
-            arrow::datatypes::DataType::List(Arc::new(typ.clone())),
+            arrow::datatypes::DataType::List(Arc::new(arrow::datatypes::Field::new(
+                "item",
+                typ.data_type().clone(),
+                typ.is_nullable(),
+            ))),
             typ.is_nullable(),
         ),
     }
@@ -171,7 +174,291 @@ fn vcf_formats_to_field(formats: Formats) -> arrow::datatypes::Field {
 
 #[cfg(test)]
 mod tests {
-    use std::sync::Arc;
+    use std::{str::FromStr, sync::Arc};
+
+    use noodles::vcf::{
+        header::{
+            record::value::{
+                map::{format, info},
+                Map,
+            },
+            Number,
+        },
+        record::genotypes,
+        record::info::field::Key,
+        Header,
+    };
+
+    use super::VCFSchemaBuilder;
+
+    #[test]
+    fn test_genotype_schema_inference() {
+        let mut header_builder = Header::builder();
+        let mut expected_fields = Vec::new();
+
+        let test_table = vec![
+            (
+                "single_int",
+                Number::Count(1),
+                format::Type::Integer,
+                arrow::datatypes::Field::new(
+                    "single_int",
+                    arrow::datatypes::DataType::Int32,
+                    false,
+                ),
+            ),
+            (
+                "single_float",
+                Number::Count(1),
+                format::Type::Float,
+                arrow::datatypes::Field::new(
+                    "single_float",
+                    arrow::datatypes::DataType::Float32,
+                    false,
+                ),
+            ),
+            (
+                "single_char",
+                Number::Count(1),
+                format::Type::Character,
+                arrow::datatypes::Field::new(
+                    "single_char",
+                    arrow::datatypes::DataType::Utf8,
+                    false,
+                ),
+            ),
+            (
+                "single_string",
+                Number::Count(1),
+                format::Type::String,
+                arrow::datatypes::Field::new(
+                    "single_string",
+                    arrow::datatypes::DataType::Utf8,
+                    false,
+                ),
+            ),
+            (
+                "single_int_array",
+                Number::Count(2),
+                format::Type::Integer,
+                arrow::datatypes::Field::new(
+                    "single_int_array",
+                    arrow::datatypes::DataType::List(Arc::new(arrow::datatypes::Field::new(
+                        "item",
+                        arrow::datatypes::DataType::Int32,
+                        false,
+                    ))),
+                    false,
+                ),
+            ),
+            (
+                "single_float_array",
+                Number::Count(2),
+                format::Type::Float,
+                arrow::datatypes::Field::new(
+                    "single_float_array",
+                    arrow::datatypes::DataType::List(Arc::new(arrow::datatypes::Field::new(
+                        "item",
+                        arrow::datatypes::DataType::Float32,
+                        false,
+                    ))),
+                    false,
+                ),
+            ),
+            (
+                "single_char_array",
+                Number::Count(2),
+                format::Type::Character,
+                arrow::datatypes::Field::new(
+                    "single_char_array",
+                    arrow::datatypes::DataType::List(Arc::new(arrow::datatypes::Field::new(
+                        "item",
+                        arrow::datatypes::DataType::Utf8,
+                        false,
+                    ))),
+                    false,
+                ),
+            ),
+        ];
+
+        for (a, b, c, d) in test_table {
+            let key = genotypes::keys::Key::from_str(a).unwrap();
+            let format = Map::builder()
+                .set_description("test")
+                .set_number(b)
+                .set_type(c)
+                .set_idx(1)
+                .build()
+                .unwrap();
+
+            header_builder = header_builder.add_format(key, format);
+
+            expected_fields.push(d);
+        }
+
+        let header = header_builder.build();
+
+        let schema = VCFSchemaBuilder::from(header).build();
+
+        let info_field = schema.field(8);
+
+        let inner_struct =
+            &arrow::datatypes::DataType::Struct(arrow::datatypes::Fields::from(expected_fields));
+        let inner_field = arrow::datatypes::Field::new("item", inner_struct.clone(), true);
+        let expected_type = arrow::datatypes::DataType::List(Arc::new(inner_field));
+
+        assert_eq!(info_field.name(), "formats");
+        assert_eq!(info_field.data_type(), &expected_type);
+    }
+
+    #[test]
+    fn test_info_schema_inference() {
+        let info_test_table = vec![
+            (
+                "single_int",
+                Number::Count(1),
+                info::Type::Integer,
+                arrow::datatypes::Field::new(
+                    "single_int",
+                    arrow::datatypes::DataType::Int32,
+                    false,
+                ),
+            ),
+            (
+                "single_str",
+                Number::Count(1),
+                info::Type::String,
+                arrow::datatypes::Field::new("single_str", arrow::datatypes::DataType::Utf8, false),
+            ),
+            (
+                "single_flag",
+                Number::Count(0),
+                info::Type::Flag,
+                arrow::datatypes::Field::new(
+                    "single_flag",
+                    arrow::datatypes::DataType::Boolean,
+                    false,
+                ),
+            ),
+            (
+                "single_char",
+                Number::Count(1),
+                info::Type::Character,
+                arrow::datatypes::Field::new(
+                    "single_char",
+                    arrow::datatypes::DataType::Utf8,
+                    false,
+                ),
+            ),
+            (
+                "single_float",
+                Number::Count(1),
+                info::Type::Float,
+                arrow::datatypes::Field::new(
+                    "single_float",
+                    arrow::datatypes::DataType::Float32,
+                    false,
+                ),
+            ),
+            (
+                "array_int",
+                Number::Count(2),
+                info::Type::Integer,
+                arrow::datatypes::Field::new(
+                    "array_int",
+                    arrow::datatypes::DataType::List(Arc::new(arrow::datatypes::Field::new(
+                        "item",
+                        arrow::datatypes::DataType::Int32,
+                        false,
+                    ))),
+                    false,
+                ),
+            ),
+            (
+                "array_str",
+                Number::Count(2),
+                info::Type::String,
+                arrow::datatypes::Field::new(
+                    "array_str",
+                    arrow::datatypes::DataType::List(Arc::new(arrow::datatypes::Field::new(
+                        "item",
+                        arrow::datatypes::DataType::Utf8,
+                        false,
+                    ))),
+                    false,
+                ),
+            ),
+            (
+                "array_char",
+                Number::Count(2),
+                info::Type::Character,
+                arrow::datatypes::Field::new(
+                    "array_char",
+                    arrow::datatypes::DataType::List(Arc::new(arrow::datatypes::Field::new(
+                        "item",
+                        arrow::datatypes::DataType::Utf8,
+                        false,
+                    ))),
+                    false,
+                ),
+            ),
+            (
+                "array_flag",
+                Number::Count(2),
+                info::Type::Flag,
+                arrow::datatypes::Field::new(
+                    "array_flag",
+                    arrow::datatypes::DataType::List(Arc::new(arrow::datatypes::Field::new(
+                        "item",
+                        arrow::datatypes::DataType::Boolean,
+                        false,
+                    ))),
+                    false,
+                ),
+            ),
+            (
+                "array_float",
+                Number::Count(2),
+                info::Type::Float,
+                arrow::datatypes::Field::new(
+                    "array_float",
+                    arrow::datatypes::DataType::List(Arc::new(arrow::datatypes::Field::new(
+                        "item",
+                        arrow::datatypes::DataType::Float32,
+                        false,
+                    ))),
+                    false,
+                ),
+            ),
+        ];
+
+        let mut fields = Vec::new();
+        let mut header = Header::builder();
+
+        for (key_str, number, ty, field) in info_test_table {
+            let key = Key::from_str(key_str).unwrap();
+            let info = Map::builder()
+                .set_description(key_str)
+                .set_number(number)
+                .set_type(ty)
+                .build()
+                .unwrap();
+
+            header = header.add_info(key, info.clone());
+            fields.push(field);
+        }
+
+        let header = header.build();
+        let schema = VCFSchemaBuilder::from(header).build();
+
+        let info_field = schema.field(7);
+
+        assert_eq!(info_field.name(), "info");
+        assert_eq!(
+            info_field.data_type(),
+            &arrow::datatypes::DataType::Struct(arrow::datatypes::Fields::from(fields))
+        );
+    }
 
     #[test]
     fn test_default_header_to_schema() {
@@ -234,8 +521,20 @@ mod tests {
             )))
         );
 
-        // TODO: better testing around info and formats
         assert_eq!(schema.field(7).name(), "info");
+        assert_eq!(
+            schema.field(7).data_type(),
+            &arrow::datatypes::DataType::Struct(arrow::datatypes::Fields::empty())
+        );
+
         assert_eq!(schema.field(8).name(), "formats");
+        assert_eq!(
+            schema.field(8).data_type(),
+            &arrow::datatypes::DataType::List(Arc::new(arrow::datatypes::Field::new(
+                "item",
+                arrow::datatypes::DataType::Struct(arrow::datatypes::Fields::empty()),
+                true
+            )))
+        );
     }
 }
