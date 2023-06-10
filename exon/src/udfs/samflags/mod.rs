f17a7e13eff5150102dc409619371c71b4d2e938
Trent Hauck <trent@trenthauck.com>
2023-06-10 16:19:33 -0700

    feat: add first samflag (#8)

---
+++ exon/src/udfs/samflags/mod.rs
@@ -0,0 +1,131 @@
+// Copyright 2023 WHERE TRUE Technologies.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+use std::sync::Arc;
+
+use arrow::array::ArrayRef;
+use datafusion::{common::cast::as_int32_array, error::Result};
+use noodles::sam::record::Flags;
+
+/// Based on the SAM flags, determine if the read is segmented.
+///
+/// # Arguments
+///
+/// * `args` - A slice of ArrayRefs. The first element should be a Int32Array.
+///     The Int32Array should contain SAM flags and be a single column.
+///
+/// # Example
+///
+/// ```rust
+/// use arrow::array::{ArrayRef, BooleanArray, Int32Array};
+/// use datafusion::{common::cast::as_boolean_array, error::Result};
+/// use std::sync::Arc;
+///
+/// let sam_flags = Int32Array::from(vec![Some(1), Some(2)]);
+/// let array_ref = Arc::new(sam_flags) as ArrayRef;
+///
+/// let result = exon::udfs::samflags::is_segmented(&[array_ref]).unwrap();
+/// let result = as_boolean_array(&result).unwrap();
+///
+/// let expected = vec![Some(true), Some(false)];
+/// let expected = BooleanArray::from(expected);
+///
+/// result
+///     .iter()
+///     .zip(expected.iter())
+///     .for_each(|(result, expected)| {
+///         assert_eq!(result, expected);
+///    });
+/// ```
+pub fn is_segmented(args: &[ArrayRef]) -> Result<ArrayRef> {
+    sam_flag_function(args, Flags::SEGMENTED)
+}
+
+/// Based on the SAM flags, determine if the read is properly aligned.
+pub fn is_properly_aligned(args: &[ArrayRef]) -> Result<ArrayRef> {
+    sam_flag_function(args, Flags::PROPERLY_ALIGNED)
+}
+
+/// Based on the SAM flags, determine if the read is unmapped.
+pub fn is_unmapped(args: &[ArrayRef]) -> Result<ArrayRef> {
+    sam_flag_function(args, Flags::UNMAPPED)
+}
+
+/// Based on the SAM flags, determine if the mate is unmapped.
+pub fn is_mate_unmapped(args: &[ArrayRef]) -> Result<ArrayRef> {
+    sam_flag_function(args, Flags::MATE_UNMAPPED)
+}
+
+/// Based on the SAM flags, determine if the read is reverse complemented.
+pub fn is_reverse_complemented(args: &[ArrayRef]) -> Result<ArrayRef> {
+    sam_flag_function(args, Flags::REVERSE_COMPLEMENTED)
+}
+
+/// Based on the SAM flags, determine if the mate is reverse complemented.
+pub fn is_mate_reverse_complemented(args: &[ArrayRef]) -> Result<ArrayRef> {
+    sam_flag_function(args, Flags::MATE_REVERSE_COMPLEMENTED)
+}
+
+/// Based on the SAM flags, determine if the read is the first segment.
+pub fn is_first_segment(args: &[ArrayRef]) -> Result<ArrayRef> {
+    sam_flag_function(args, Flags::FIRST_SEGMENT)
+}
+
+/// Based on the SAM flags, determine if the read is the last segment.
+pub fn is_last_segment(args: &[ArrayRef]) -> Result<ArrayRef> {
+    sam_flag_function(args, Flags::LAST_SEGMENT)
+}
+
+/// Based on the SAM flags, determine if the secondary alignment.
+pub fn is_secondary(args: &[ArrayRef]) -> Result<ArrayRef> {
+    sam_flag_function(args, Flags::SECONDARY)
+}
+
+/// Based on the SAM flags, determine if the read is a quality check failure.
+pub fn is_qc_fail(args: &[ArrayRef]) -> Result<ArrayRef> {
+    sam_flag_function(args, Flags::QC_FAIL)
+}
+
+/// Based on the SAM flags, determine if the read is a PCR or optical duplicate.
+pub fn is_duplicate(args: &[ArrayRef]) -> Result<ArrayRef> {
+    sam_flag_function(args, Flags::DUPLICATE)
+}
+
+/// Based on the SAM flags, determine if the read is supplementary.
+pub fn is_supplementary(args: &[ArrayRef]) -> Result<ArrayRef> {
+    sam_flag_function(args, Flags::SUPPLEMENTARY)
+}
+
+fn sam_flag_function(args: &[ArrayRef], record_flag: Flags) -> Result<ArrayRef> {
+    if args.len() != 1 {
+        return Err(datafusion::error::DataFusionError::Execution(
+            "flag scalar takes one argument".to_string(),
+        ));
+    }
+
+    let sam_flags = as_int32_array(&args[0])?;
+
+    let array = sam_flags
+        .iter()
+        .map(|sam_flag| {
+            sam_flag.map(|f| {
+                let f16 = f as u16;
+                let flag = Flags::from_bits_truncate(f16);
+                flag.contains(record_flag)
+            })
+        })
+        .collect::<arrow::array::BooleanArray>();
+
+    Ok(Arc::new(array))
+}
