ae96d79f58c0e7fdc9e0b6f260a65b47a7914379
Trent Hauck <trent@trenthauck.com>
2023-06-29 08:56:20 -0700

    feat(r): updating ffi

--- exon-r/exonr/R/main.R
+++ exon-r/exonr/R/main.R
@@ -1,13 +1,19 @@
 # Import the wrappers
 
-require(arrow)
+library(arrow)
+library(nanoarrow)
 
 read_fasta_table <- function(file_path) {
-    stream_ptr <- arrow$allocate_arrow_array_stream()
+    stream <- nanoarrow::nanoarrow_allocate_array_stream()
 
-    batch_reader_ptr <- read_fasta_file_extendr(file_path, stream_ptr)
+    batch_reader_ptr <- read_fasta_file_extendr(
+        file_path,
+        nanoarrow::nanoarrow_pointer_addr_chr(stream)
+    )
 
-    record_batch_reader <- RecordBatchStreamReader$import_from_c(batch_reader_ptr)
+    record_batch_reader <- RecordBatchStreamReader$import_from_c(
+        batch_reader_ptr
+    )
 
     print(record_batch_reader)
 }
