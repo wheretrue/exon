5296041008761100e0164f051d2913c546d86bd7
Trent Hauck <trent@trenthauck.com>
2023-06-29 20:58:09 -0700

    feat: refactor r functions (#28)
    * feat: refactor r functions
    * fix: fix build script

--- exon-r/exonr/R/main.R
+++ exon-r/exonr/R/main.R
@@ -1,22 +1,61 @@
-# Import the wrappers
+#' Defines the main functions for the exonr package.
 
 library(arrow)
 library(nanoarrow)
 
-read_fasta_table <- function(file_path) {
+#' Read FASTA File
+#'
+#' This function reads a FASTA file from a file and returns a RecordBatch
+#' stream.
+#'
+#' @param file_path The path to the FASTA file.
+#'
+#' @return A RecordBatch stream representing the contents of the FASTA table.
+#'
+#' @export
+read_fasta_file <- function(file_path) {
     stream <- nanoarrow::nanoarrow_allocate_array_stream()
+    pointer_addr <- nanoarrow::nanoarrow_pointer_addr_chr(stream)
 
-    read_fasta_file_extendr(
-        file_path,
-        nanoarrow::nanoarrow_pointer_addr_chr(stream)
-    )
+    read_inferred_exon_table(file_path, pointer_addr)
 
-    record_batch_reader <- RecordBatchStreamReader$import_from_c(
-        nanoarrow::nanoarrow_pointer_addr_chr(stream)
-    )
+    return(RecordBatchStreamReader$import_from_c(pointer_addr))
+}
 
-    tab <- record_batch_reader$read_table()
-    df <- as.data.frame(tab)
+#' Read FASTQ File
+#'
+#' This function reads a FASTQ file from a file and returns a RecordBatch
+#' stream.
+#'
+#' @param file_path The path to the FASTQ file.
+#'
+#' @return A RecordBatch stream representing the contents of the FASTQ table.
+#'
+#' @export
+read_fastq_file <- function(file_path) {
+    stream <- nanoarrow::nanoarrow_allocate_array_stream()
+    pointer_addr <- nanoarrow::nanoarrow_pointer_addr_chr(stream)
 
-    print(df)
+    read_inferred_exon_table(file_path, pointer_addr)
+
+    return(RecordBatchStreamReader$import_from_c(pointer_addr))
+}
+
+#' Read GFF File
+#'
+#' This function reads a GFF file from a file and returns a RecordBatch
+#' stream.
+#'
+#' @param file_path The path to the GFF file.
+#'
+#' @return A RecordBatch stream representing the contents of the GFF table.
+#'
+#' @export
+read_gff_file <- function(file_path) {
+    stream <- nanoarrow::nanoarrow_allocate_array_stream()
+    pointer_addr <- nanoarrow::nanoarrow_pointer_addr_chr(stream)
+
+    read_inferred_exon_table(file_path, pointer_addr)
+
+    return(RecordBatchStreamReader$import_from_c(pointer_addr))
 }
--- exon-r/exonr/src/rust/src/lib.rs
+++ exon-r/exonr/src/rust/src/lib.rs
@@ -4,13 +4,13 @@ use datafusion::{arrow::ffi_stream::FFI_ArrowArrayStream, prelude::SessionContex
 use exon::{context::ExonSessionExt, ffi::create_dataset_stream_from_table_provider};
 use extendr_api::prelude::*;
 
-fn read_fasta_file_inner(path: &str, stream_ptr: *mut FFI_ArrowArrayStream) {
+fn read_inferred_exon_table_inner(path: &str, stream_ptr: *mut FFI_ArrowArrayStream) {
     let rt = Arc::new(tokio::runtime::Runtime::new().unwrap());
 
     let ctx = SessionContext::new();
 
     rt.block_on(async {
-        let df = ctx.read_fasta(path, None).await.unwrap();
+        let df = ctx.read_inferred_exon_table(path).await.unwrap();
 
         create_dataset_stream_from_table_provider(df, rt.clone(), stream_ptr)
             .await
@@ -18,21 +18,19 @@ fn read_fasta_file_inner(path: &str, stream_ptr: *mut FFI_ArrowArrayStream) {
     });
 }
 
-/// Return string `"Hello world!"` to R.
+/// Copy the inferred exon table from the given path into the given stream.
 /// @export
 #[extendr]
-fn read_fasta_file_extendr(file_path: &str, stream_ptr: &str) {
+fn read_inferred_exon_table(file_path: &str, stream_ptr: &str) {
     let stream_out_ptr_addr: usize = stream_ptr.parse().unwrap();
 
     let stream_out_ptr = stream_out_ptr_addr as *mut FFI_ArrowArrayStream;
 
-    read_fasta_file_inner(file_path, stream_out_ptr);
-
-    eprintln!("read_fasta_file_extendr");
+    read_inferred_exon_table_inner(file_path, stream_out_ptr);
 }
 
 extendr_module! {
     mod exonr;
 
-    fn read_fasta_file_extendr;
+    fn read_inferred_exon_table;
 }
