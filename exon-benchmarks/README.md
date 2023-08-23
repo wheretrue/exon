646a42acbcd5d40813e336174c1e54616b6ed997
Trent Hauck <trent@trenthauck.com>
2023-08-23 11:22:56 -0700

    updates

--- exon-benchmarks/README.md
+++ exon-benchmarks/README.md
@@ -2,8 +2,6 @@
 
 This page describes the benchmarks for Exon.
 
-Please note, some benchmarks aren't apples to apples. For example, counting the results of a VCF query through `bcftools` will not generate an otherwise useful data structure (Arrow RecordBatch) outside of the task at hand and thus speed differences should be considered in that context.
-
 ## UniProt Non-Methionine Amino Acid Counts
 
 This benchmarks shows the performance of counting the number of amino acids from uniprot that do not start with methionine.
@@ -14,7 +12,7 @@ UniProt has about 560k sequences.
 
 ## VCF Query
 
-This benchmark shows querying a 2.6G VCF file for a genomic region. A reminder that Exon is parsing the VCF file and its fields into a RecordBatch with Schema for Info and Format fields.
+This benchmark shows querying a 2.6G VCF file for a genomic region.
 
 ![VCF Scan](./plots/vcf-query.svg)
 
---
+++ exon/src/optimizer/vcf_region_optimizer_rule.rs
@@ -0,0 +1,151 @@
+// Copyright 2023 WHERE TRUE Technologies.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+use std::sync::Arc;
+
+use datafusion::common::tree_node::{Transformed, TreeNode};
+use datafusion::error::Result;
+use datafusion::logical_expr::{BinaryExpr, Operator};
+use datafusion::physical_optimizer::PhysicalOptimizerRule;
+use datafusion::physical_plan::filter::FilterExec;
+use datafusion::physical_plan::ExecutionPlan;
+use datafusion::prelude::*;
+use noodles::core::region::Interval;
+use noodles::core::{Position, Region};
+
+use crate::datasources::vcf::{self, VCFScan};
+
+fn chrom_operator(expr: &BinaryExpr) -> Option<String> {
+    match (expr.left.as_ref(), expr.right.as_ref(), expr.op) {
+        (Expr::Column(left), Expr::Literal(right), Operator::Eq) => {
+            if left.name != "chrom" {
+                return None;
+            } else {
+                return Some(right.to_string());
+            }
+        }
+        (_, _, _) => None,
+    }
+}
+
+fn position_operator(expr: &BinaryExpr) -> Option<usize> {
+    match (expr.left.as_ref(), expr.right.as_ref(), expr.op) {
+        (Expr::Column(left), Expr::Literal(right), Operator::Eq) => {
+            if left.name != "pos" {
+                return None;
+            } else {
+                return Some(right.to_string().parse::<usize>().unwrap());
+            }
+        }
+        (_, _, _) => None,
+    }
+}
+
+fn optimize(plan: Arc<dyn ExecutionPlan>) -> Result<Transformed<Arc<dyn ExecutionPlan>>> {
+    // if we get a FilterExec with the correct expression and its input is a VCFScan, we can
+    // replace the FilterExec with a VCFScan with the same expression and push down the filter
+
+    if let Some(filter_exec) = plan.as_any().downcast_ref::<FilterExec>() {
+        let vcf_scan = filter_exec.input().as_any().downcast_ref::<VCFScan>();
+
+        filter_exec.predicate();
+
+        let pred = filter_exec
+            .predicate()
+            .as_any()
+            .downcast_ref::<datafusion::physical_expr::expressions::BinaryExpr>();
+
+        match (vcf_scan, pred) {
+            (Some(scan), Some(pred)) => {
+                let left_pred = pred
+                    .left()
+                    .as_any()
+                    .downcast_ref::<datafusion::physical_expr::expressions::BinaryExpr>()
+                    .unwrap();
+
+                // let left_column = left_pred
+                //     .left()
+                //     .as_any()
+                //     .downcast_ref::<datafusion::physical_expr::expressions::Column>()
+                //     .unwrap();
+                let right_literal = left_pred
+                    .right()
+                    .as_any()
+                    .downcast_ref::<datafusion::physical_expr::expressions::Literal>()
+                    .unwrap();
+
+                let chrom = Some("chr1".to_string());
+
+                // let chrom = match right_literal.value() {
+                //     Expr::BinaryExpr(left) => chrom_operator(left),
+                //     _ => None,
+                // };
+
+                // let pos = match pred.right.as_ref() {
+                //     Expr::BinaryExpr(right) => {
+                //         position_operator(right).map(|p| Position::new(p).unwrap())
+                //     }
+                //     _ => None,
+                // };
+
+                let pos = Some(Position::new(1).unwrap());
+
+                match (chrom, pos) {
+                    (Some(chrom), Some(pos)) => {
+                        let start = pos;
+                        let end = pos;
+                        let interval = Interval::from(start..=end);
+
+                        let region = Region::new(chrom, interval);
+
+                        let new_scan = scan.clone().with_filter(region);
+
+                        return Ok(Transformed::Yes(Arc::new(new_scan)));
+                    }
+                    (_, _) => return Ok(Transformed::No(plan)),
+                }
+            }
+            (_, _) => return Ok(Transformed::No(plan)),
+        }
+    }
+
+    Ok(Transformed::No(plan))
+}
+
+#[derive(Default)]
+pub struct ExonVCFRegionOptimizer {}
+
+impl PhysicalOptimizerRule for ExonVCFRegionOptimizer {
+    fn optimize(
+        &self,
+        plan: std::sync::Arc<dyn ExecutionPlan>,
+        _config: &datafusion::config::ConfigOptions,
+    ) -> datafusion::error::Result<std::sync::Arc<dyn ExecutionPlan>> {
+        eprintln!("ExonVCFRegionOptimizer::optimize");
+
+        let plan = optimize(plan)?;
+
+        let (plan, _transformed) = plan.into_pair();
+
+        Ok(plan)
+    }
+
+    fn name(&self) -> &str {
+        "exon_vcf_region_optimizer_rule"
+    }
+
+    fn schema_check(&self) -> bool {
+        true
+    }
+}
