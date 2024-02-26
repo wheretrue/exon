adc15c9cc2045d11c788705ad3768ec6465d01f9
Trent Hauck <trent@trenthauck.com>
2024-02-25 20:55:43 -0800

    test: add test for indexed scan

--- bin/test.sh
+++ bin/test.sh
@@ -38,8 +38,12 @@ sleep 2
 # Create the test bucket.
 aws --endpoint-url=http://localhost:4566 s3 mb s3://test-bucket
 
-# Upload the test data.
+# Upload the test data
 aws --endpoint-url=http://localhost:4566 s3 cp ./exon/exon-core/test-data/datasources/fasta/test.fasta s3://test-bucket/test.fasta
+
+aws --endpoint-url=http://localhost:4566 s3 cp ./exon/exon-core/test-data/datasources/fasta-indexed/test.fasta s3://test-bucket/test-indexed.fasta
+aws --endpoint-url=http://localhost:4566 s3 cp ./exon/exon-core/test-data/datasources/fasta-indexed/test.fasta.fai s3://test-bucket/test-indexed.fasta.fai
+
 
 # Make the bucket public.
 aws --endpoint-url=http://localhost:4566 s3api put-bucket-acl --bucket test-bucket --acl public-read
